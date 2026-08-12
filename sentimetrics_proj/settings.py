ba490118c6326fcffa6025233baec0b48d7b6dad
Zarion-07 <y1746579@gmail.com>
2026-08-12 14:51:42 +0530

    Html changes

--- sentimetrics_proj/settings.py
+++ sentimetrics_proj/settings.py
@@ -1,5 +1,3 @@
-
-
 import os
 from pathlib import Path
 
@@ -54,18 +52,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "sentimetrics_proj.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/6.1/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-
 # Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
